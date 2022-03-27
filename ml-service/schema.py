a8d07835c9120acb00f594cb9518b8c7cffa6097
bpht <bennyphtam@gmail.com>
2022-03-27 21:26:07 +0800

    Deleted ModelType Enum

--- ml-service/schema.py
+++ ml-service/schema.py
@@ -1,16 +1,9 @@
-from enum import Enum
 from typing import Optional, List
 
 from pydantic import BaseModel
-from core import model
-
-
-class ModelType(str, Enum):
-    linear_regressor = model.Linear.__name__
-    random_forest = model.RandomForest.__name__
 
 
 class ModelInput(BaseModel):
-    model_name: ModelType
+    model_name: str
     target_col: str
     feature_cols: Optional[List[str]] = None
