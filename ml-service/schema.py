c099dfbab7fba9cf6f0203e1a911c5c80d135813
bpht <bennyphtam@gmail.com>
2022-03-28 21:42:51 +0800

    Added framework & params as body request

--- ml-service/schema.py
+++ ml-service/schema.py
@@ -1,9 +1,16 @@
 from typing import Optional, List
-
+from enum import Enum
 from pydantic import BaseModel
 
 
+class ModelFrameWork(str, Enum):
+    sklearn = "sklearn"
+
+
 class ModelInput(BaseModel):
+    framework: ModelFrameWork = ModelFrameWork.sklearn
     model_name: str
+    params: dict = {}
     target_col: str
     feature_cols: Optional[List[str]] = None
+
